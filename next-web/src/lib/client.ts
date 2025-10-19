b1ba6065b55557d23ada6690f0094c476d9ec0d1
yy <free.tkdysk@gmail.com>
2025-10-20 02:49:59 +0900

    add fix & lint command

--- next-web/src/lib/client.ts
+++ next-web/src/lib/client.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-import { env } from '@/config'
+import { env } from "@/config";
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+  prisma: PrismaClient;
+};
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma
+export default prisma;
